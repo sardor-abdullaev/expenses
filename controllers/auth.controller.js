295e775a14ad7d3df8228abe14b7f72e9c33cd7e
Sardor Abdullaev <abdullaevss94@mail.ru>
2024-02-21 00:07:24 +0500

    update user password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const User = require("../models/user.model");
 const catchAsync = require("../utils/catchAsync");
 const jwt = require("jsonwebtoken");
 const AppError = require("../utils/appError");
+const { promisify } = require("util");
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -59,4 +60,84 @@ exports.login = catchAsync(async (req, res, next) => {
   }
   //   3) if everything ok, send token to client
   createSendToken(user, 200, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access!")
+    );
+  }
+
+  // 2) ValVerification token
+  // jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+  //   console.log(decoded);
+  // });
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError("The user belonging to this token does no longer exist", 401)
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
+  next();
+});
+
+exports.restictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not permission to perform this action", 403)
+      );
+    }
+    next();
+  };
+};
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) Get user from collection
+  const user = await User.findById(req.user._id).select("+password");
+
+  // 2) Check if POSTed current password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(
+      new AppError("Entered password is wrong! You can reach admin", 401)
+    );
+  }
+
+  // 3) if so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  // 4) Log user in, send jwt
+  createSendToken(user, 200, res);
+});
+
+exports.resetToDefaultPassword = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.body.userId);
+  if (!user) {
+    return new AppError("no user", 401);
+  }
+  user.password = "test123";
+  user.passwordConfirm = "test123";
+
+  await user.save();
+  res.status(200).json({
+    status: "success",
+  });
 });
--- routes/user.router.js
+++ routes/user.router.js
@@ -9,9 +9,25 @@ router.post("/login", authController.login);
 
 router
   .route("/")
-  .get(userController.getAllUsers)
+  .get(
+    authController.protect,
+    authController.restictTo("admin"),
+    userController.getAllUsers
+  )
   .post(userController.createUser);
-  
+
+router
+  .route("/resetDefaultPassword")
+  .patch(
+    authController.protect,
+    authController.restictTo("admin"),
+    authController.resetToDefaultPassword
+  );
+
+router
+  .route("/updateMyPassword")
+  .patch(authController.protect, authController.updatePassword);
+
 router.route("/:id").get(userController.getUser);
 
 module.exports = router;
